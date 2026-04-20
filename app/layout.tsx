afd9299e24a1d0084c6befd77c6119e4811cf808
dilekhashakthi <2000dilekha@gmail.com>
2026-04-20 20:26:28 +0530

    define root layout and contants

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../assets/styles/globals.css";
+import { APP_NAME, APP_DESCRIPTION, SERVER_URL } from "@/lib/constants";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "ProStore",
-  description: "A modern E commerce platform built with Next.js and TypeScript",
+  title: {
+    template: `%s | ProStore`,
+    default: APP_NAME,
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL),
 };
 
 export default function RootLayout({
---
+++ lib/constants/index.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "ProStore";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "A modern E commerce platform built with Next.js and TypeScript";
+export const SERVER_URL = process.env.NEXT_PUBLIC_SEVER_URL || "https://localhost:3000"
