232e86e938cc33d4e2c6e9fef63ed4cb0480a127
dilekhashakthi <2000dilekha@gmail.com>
2026-04-21 00:52:41 +0530

    add dark/light theme toggle mode

---
+++ components/shared/Header/mode-toggle.tsx
@@ -0,0 +1,63 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { SunIcon, MoonIcon, SunMoon } from "lucide-react";
+import { useTheme } from "next-themes";
+
+const ModeToggle = () => {
+  const { theme, setTheme, resolvedTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          className="border-none outline-none focus:outline-none focus:ring-0 
+                     focus-visible:ring-0 focus-visible:ring-offset-0 
+                     focus-visible:outline-none"
+        >
+          {!resolvedTheme ? (
+            <SunMoon />
+          ) : theme === "system" ? (
+            <SunMoon />
+          ) : resolvedTheme === "dark" ? (
+            <MoonIcon />
+          ) : (
+            <SunIcon />
+          )}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="center">
+        <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuCheckboxItem
+          checked={theme === "system"}
+          onClick={() => setTheme("system")}
+        >
+          System
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          checked={theme === "dark"}
+          onClick={() => setTheme("dark")}
+        >
+          Dark
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          checked={theme === "light"}
+          onClick={() => setTheme("light")}
+        >
+          Light
+        </DropdownMenuCheckboxItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default ModeToggle;
