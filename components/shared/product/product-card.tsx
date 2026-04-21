7e6b8e092779f478ee7049843a27984ab5d71217
dilekhashakthi <2000dilekha@gmail.com>
2026-04-21 23:49:11 +0530

    add ProductPrice component and integrate it into ProductCard

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
+import ProductPrice from "./Product-price";
 
 const ProductCard = ({
   product,
@@ -29,7 +30,7 @@ const ProductCard = ({
         <div className="flex-between gap-4">
             <p>{product.rating} Stars</p>
             {product.stock > 0 ? (
-                <p className="font-bold">{product.price}</p>
+                <ProductPrice value={Number(product.price)} />
             ) : (
                 <p className="text-destructive">Out of Stock</p>
             )}
