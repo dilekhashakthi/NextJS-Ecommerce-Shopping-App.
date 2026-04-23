b3be061b7d1d2085601a2e93efc8f8945973cd11
dilekhashakthi <2000dilekha@gmail.com>
2026-04-23 22:37:07 +0530

    implement getLatestProducts function to fetch latest products from database refactor: update HomePage to use fetched products instead of sample data chore: add LATEST_PRODUCT_LIMIT constant for product fetching limit

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -3,3 +3,4 @@ export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
   "A modern E commerce platform built with Next.js and TypeScript";
 export const SERVER_URL = process.env.NEXT_PUBLIC_SEVER_URL || "https://localhost:3000"
+export const LATEST_PRODUCT_LIMIT = Number(process.env.LATEST_PRODUCT_LIMIT) || 4
