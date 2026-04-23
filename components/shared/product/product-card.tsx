a0ff0e7fe0bf99c1fdb76f843e77c88cdf8577a1
dilekhashakthi <2000dilekha@gmail.com>
2026-04-23 23:16:22 +0530

    enhance product handling with type safety and validation

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -2,13 +2,9 @@ import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
 import ProductPrice from "./Product-price";
+import { Product } from "@/types";
 
-const ProductCard = ({
-  product,
-}: {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  product: any;
-}) => {
+const ProductCard = ({ product }: { product: Product }) => {
   return (
     <Card className="w-full max-w-sm">
       <CardHeader className="p-0 items-center">
@@ -28,12 +24,12 @@ const ProductCard = ({
           <h2 className="text-sm font-medium">{product.name}</h2>
         </Link>
         <div className="flex-between gap-4">
-            <p>{product.rating} Stars</p>
-            {product.stock > 0 ? (
-                <ProductPrice value={Number(product.price)} />
-            ) : (
-                <p className="text-destructive">Out of Stock</p>
-            )}
+          <p>{product.rating} Stars</p>
+          {product.stock > 0 ? (
+            <ProductPrice value={Number(product.price)} />
+          ) : (
+            <p className="text-destructive">Out of Stock</p>
+          )}
         </div>
       </CardContent>
     </Card>
