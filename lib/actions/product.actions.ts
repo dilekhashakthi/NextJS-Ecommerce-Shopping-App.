b98c3e8a957893310022970c7d5f9e6b7932dfc1
dilekhashakthi <2000dilekha@gmail.com>
2026-04-25 10:09:25 +0530

    integrate Prisma with Neon and extend product model for price and rating transformation

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,15 +1,11 @@
 "use server";
 
-import { PrismaClient } from "../generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import {prisma} from '@/db/prisma'
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCT_LIMIT } from "../constants";
 
 // Get latest products
 export async function getLatestProducts() {
-  const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
-  const prisma = new PrismaClient({ adapter });
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCT_LIMIT,
     orderBy: { createdAt: "desc" },
