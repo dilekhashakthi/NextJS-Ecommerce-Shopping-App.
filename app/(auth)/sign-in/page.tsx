335f96dded1797a5cb5ddb1ef3a4489427f0ee2e
dilekhashakthi <dilekhashakthi00@gmail.com>
2026-05-02 23:07:34 +0530

    feat(auth): create SignInPage component for user authentication

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,39 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/public/images/logo.svg";
+import { APP_NAME } from "@/lib/constants";
+
+export const metadata: Metadata = {
+  title: "Sign In",
+};
+
+const SignInPage = () => {
+  return (
+    <div className="w-full max-w-md mx-auto">
+      <Card>
+        <CardHeader className="space-y-4">
+          <Link href="/" className="flex-center">
+            <Image
+              src={Logo}
+              alt={`${APP_NAME} logo`}
+              width={100}
+              height={100}
+              priority={true}
+            />
+          </Link>
+          <CardTitle className="text-center">Sign In</CardTitle>
+          <CardDescription className="text-center">
+            Sign in to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+              {/* Sign in form goes here */}
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default SignInPage;
