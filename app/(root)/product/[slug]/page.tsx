f2a654b83beefcbec4a23e0aca93591f9c4a5080
dilekhashakthi <dilekhashakthi00@gmail.com>
2026-05-06 19:21:22 +0530

    add AddToCart component and integrate with product details page; include Toaster for notifications

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -2,9 +2,9 @@ import { getProductBySlug } from "@/lib/actions/product.actions";
 import { notFound } from "next/navigation";
 import { Badge } from "@/components/ui/badge";
 import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
 import ProductPrice from "@/components/shared/product/Product-price";
 import ProductImages from "@/components/shared/product/product-images";
+import AddToCart from "@/components/shared/product/add-to-cart";
 
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
@@ -66,7 +66,16 @@ const ProductDetailsPage = async (props: {
                 </div>
                 {product.stock > 0 && (
                   <div className="flex-center">
-                    <Button className="w-full">Add To Cart</Button>
+                    <AddToCart
+                    item={{
+                      productId: product.id,
+                      name: product.name,
+                      slug:product.slug,
+                      price: product.price,
+                      qty: 1,
+                      image: product.images![0]
+                    }}
+                    />
                   </div>
                 )}
               </CardContent>
