f83748c7cdcf89d55d7c6dab71e160630a3d6b2c
dilekhashakthi <dilekhashakthi00@gmail.com>
2026-05-22 22:18:39 +0530

    add admin layout, main navigation, and user role handling in user button

---
+++ app/admin/layout.tsx
@@ -0,0 +1,43 @@
+import Menu from "@/components/shared/Header/menu";
+import UserButton from "@/components/shared/Header/user-button";
+import { APP_NAME } from "@/lib/constants";
+import Image from "next/image";
+import Link from "next/link";
+import MainNav from "./main-nav";
+import { Input } from "@/components/ui/input";
+
+export default function AminLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <div className="flex flex-col">
+        <div className="border-b container mx-auto">
+          <div className="flex items-center h-16 px-6">
+            <Link href="/" className="w-22">
+              <Image
+                src="/images/logo.svg"
+                height={48}
+                width={48}
+                alt={APP_NAME}
+              />
+            </Link>
+            <MainNav className="mx-6" />
+            <div className="ml-auto items-center flex space-x-4">
+              <div>
+                <Input type="search" placeholder="Search..." className="md:w-25 lg:w-75" />
+              </div>
+              <Menu userButton={<UserButton />} />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex-1 space-y-4 p-8 pt-8 container mx-auto">
+          {children}
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ app/admin/overview/page.tsx
@@ -0,0 +1,5 @@
+const AdminOverviewPage = () => {
+  return <>overview</>;
+};
+
+export default AdminOverviewPage;
