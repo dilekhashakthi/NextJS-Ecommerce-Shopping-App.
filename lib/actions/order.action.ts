3d9ed4f5ecaac9a8f3909d11ee1805a2e51220bc
dilekhashakthi <dilekhashakthi00@gmail.com>
2026-05-22 23:08:55 +0530

    add sales data aggregation and order summary retrieval functions

--- lib/actions/order.action.ts
+++ lib/actions/order.action.ts
@@ -11,6 +11,7 @@ import { CartItem, PaymentResult } from "@/types";
 import { paypal } from "../paypal";
 import { revalidatePath } from "next/cache";
 import { PAGE_SIZE } from "../constants";
+import { Prisma } from "../generated/prisma/client";
 
 // Create order and create the order items
 export async function createOrder() {
@@ -284,11 +285,59 @@ export async function getMyOrders({
   });
 
   const dataCount = await prisma.order.count({
-    where: {userId: session?.user?.id},
-  })
+    where: { userId: session?.user?.id },
+  });
 
   return {
     data,
-    totalPages: Math.ceil(dataCount / limit)
+    totalPages: Math.ceil(dataCount / limit),
+  };
+}
+
+type SalesDataType = {
+  month: string;
+  totalSales: number;
+}[];
+
+// Get sales data and order summary
+export async function getOrderSummary() {
+  // Get counts for each resource
+  const ordersCount = await prisma.order.count();
+  const productsCount = await prisma.product.count();
+  const usersCount = await prisma.user.count();
+
+  // Calculate the total sales
+  const totalSales = await prisma.order.aggregate({
+    _sum: { totalPrice: true },
+  });
+
+  // Get monthly sales
+  const saleDataRaw = await prisma.$queryRaw<
+    Array<{ month: string; totalSales: Prisma.Decimal }>
+  >`SELECT to_char("createdAt", "MM/YY") as "month", sum("totalPrice") as "totalSales" FROM "Order" GROUP BY to_char("createdAt", "MM/YY")`;
+
+  const salesData = saleDataRaw.map((entry) => ({
+    month: entry.month,
+    totalSales: Number(entry.totalSales),
+  }));
+
+  // Get latest sales
+  const latestSales = await prisma.order.findMany({
+    orderBy: { createdAt: "desc" },
+    include: {
+      user: {
+        select: { name: true },
+      },
+    },
+    take: 6,
+  });
+
+  return {
+    ordersCount,
+    productsCount,
+    usersCount,
+    totalSales,
+    latestSales,
+    salesData
   }
 }
