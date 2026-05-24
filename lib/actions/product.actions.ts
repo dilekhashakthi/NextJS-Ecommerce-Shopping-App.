08a98d0f2b75008026e00bb8a1f4f872d31d7158
dilekhashakthi <dilekhashakthi00@gmail.com>
2026-05-25 00:20:14 +0530

    create AdminProductUpdatePage for updating products; implement getProductById function

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -24,6 +24,15 @@ export async function getProductBySlug(slug: string) {
   });
 }
 
+// Get single product by it's ID
+export async function getProductById(productId: string) {
+  const data = await prisma.product.findFirst({
+    where: { id: productId },
+  });
+
+  return convertToPlainObject(data);
+}
+
 // Get all products
 export async function getAllProducts({
   query,
@@ -37,7 +46,7 @@ export async function getAllProducts({
   category?: string;
 }) {
   const data = await prisma.product.findMany({
-    orderBy: {createdAt: 'desc'},
+    orderBy: { createdAt: "desc" },
     skip: (page - 1) * limit,
     take: limit,
   });
