a979a77a0de6feb892bca9473d7bc792b0531517
dilekhashakthi <dilekhashakthi00@gmail.com>
2026-05-24 19:17:30 +0530

    add file upload functionality with image uploader and route handler; implement upload button and dropzone components

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   serverExternalPackages: ["@prisma/client", "prisma"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+        port: ''
+      }
+    ]
+  }
 };
 
 export default nextConfig;
