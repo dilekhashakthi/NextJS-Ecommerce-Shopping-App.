f75d71688efdfb878691155d97ac2e6447b40ce7
dilekhashakthi <dilekhashakthi00@gmail.com>
2026-05-25 22:20:31 +0530

    add deleteUser function to user actions and integrate with AdminUserPage

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -10,7 +10,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { getAllUsers } from "@/lib/actions/user.actions";
+import { deleteUser, getAllUsers } from "@/lib/actions/user.actions";
 import { requireAdmin } from "@/lib/auth-guard";
 import { formatId } from "@/lib/utils";
 import Link from "next/link";
@@ -60,7 +60,7 @@ const AdminUserPage = async (props: {
                       <span className="px-2">Details</span>
                     </Link>
                   </Button>
-                  {/* <DeleteDialog id={user.id} action={deleteUser} /> */}
+                  <DeleteDialog id={user.id} action={deleteUser} />
                 </TableCell>
               </TableRow>
             ))}
