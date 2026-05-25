ea1165a850932663a6e0871b1b5e34eb16dd0da2
dilekhashakthi <dilekhashakthi00@gmail.com>
2026-05-25 23:24:05 +0530

    refactor UpdateUserForm props structure for clarity in AdminUserUpdatePage

--- app/admin/users/[id]/page.tsx
+++ app/admin/users/[id]/page.tsx
@@ -20,7 +20,14 @@ const AdminUserUpdatePage = async (props: {
   return (
     <div className="space-y-8 max-w-lg mx-auto">
       <h1 className="h2-bold">Update User</h1>
-      <UpdateUserForm user={user} />
+      <UpdateUserForm
+        user={{
+          id: user.id,
+          name: user.name ?? "",
+          email: user.email ?? "",
+          role: user.role,
+        }}
+      />
     </div>
   );
 };
