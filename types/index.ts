c59aa2a7e893266a60e983755eff656a9a104037
dilekhashakthi <dilekhashakthi00@gmail.com>
2026-06-03 20:38:01 +0530

    add review functionality; create Review model, validation schema, and default values

--- types/index.ts
+++ types/index.ts
@@ -5,6 +5,7 @@ import {
   insertOrderItemSchema,
   insertOrderSchema,
   insertProductSchema,
+  insertReviewSchema,
   paymentResultSchema,
   shippingAddressSchema,
 } from "@/lib/validators";
@@ -30,3 +31,8 @@ export type Order = Omit<z.infer<typeof insertOrderSchema>, "userId"> & {
   user: { name: string | null; email: string | null } | null;
 };
 export type PaymentResult = z.infer<typeof paymentResultSchema>
+export type Review = z.infer<typeof insertReviewSchema> & {
+  id: string;
+  createdAt: Date;
+  user?: { name: string };
+};
